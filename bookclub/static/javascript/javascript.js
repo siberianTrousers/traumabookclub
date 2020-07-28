793f1b29030ca5561f76f6b9e843d00570647c71
JenRam <jram.work20@gmail.com>
2020-07-28 16:45:05 -0700

    STYLE ME TIMBERS

---
+++ bookclub/static/javascript/javascript.js
@@ -0,0 +1,6 @@
+function openNav() {
+  document.getElementById("homePageSideNav").style.width = "250px";
+}
+function closeNav() {
+  document.getElementById("homePageSideNav").style.width = "0";
+}
