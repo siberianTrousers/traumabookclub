8228b0b31228c42e17f5128e4ae2d30475cca701
JenRam <jram.work20@gmail.com>
2020-08-08 08:33:35 -0700

    Added new pages and a modal window

--- bookclub/static/js/javascript.js
+++ bookclub/static/js/javascript.js
@@ -1,6 +1,24 @@
+// SIDE NAV
 function openNav() {
   document.getElementById("homePageSideNav").style.width = "250px";
 }
 function closeNav() {
   document.getElementById("homePageSideNav").style.width = "0";
 }
+
+// MODAL
+var modal = document.getElementById("donateModal");
+var modalBtn = document.getElementById("donateBtn");
+var span = document.getElementsByClassName("close")[0];
+
+modalBtn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+// window.onclick = function(event) {
+//   if(event.target ==  modal) {
+//     modal.style.display = "none";
+//   }
+// }
